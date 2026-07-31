f78bc9f984b96ea55afc5ae5c95cdcdc99a83f0d
dionmar-lariosa <main.dionmar@gmail.com>
2026-07-31 18:53:06 +0800

    -- DB config

---
+++ database/pool.js
@@ -0,0 +1,15 @@
+import { Pool } from "pg";
+import { configDotenv } from "dotenv";
+
+configDotenv({
+  path: `.env.${process.env.NODE_ENV ?? "development"}`,
+  debug: process.env.IS_DEBUG
+});
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 20,
+  idleTimeoutMillis: 1000,
+  connectionTimeoutMillis: 1000,
+  maxUses: 7500
+});
